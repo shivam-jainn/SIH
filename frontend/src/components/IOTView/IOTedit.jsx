4c5a95bac14232a24ce2fb65acc18fa88a659d61
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-15 14:29:15 +0530

    Update UI

--- frontend/src/components/IOTView/IOTedit.jsx
+++ frontend/src/components/IOTView/IOTedit.jsx
@@ -8,51 +8,97 @@ import {
   Subtitle,
   Title,
 } from "@tremor/react";
-import React from "react";
+import React, { useRef, useState } from "react";
 import NavBar from "../Navbar/Navbar";
 
 const IOTedit = () => {
+  const fileInputRef = useRef(null);
+  const [selectedFileName, setSelectedFileName] = useState("");
+  const [fileUploaded, setFileUploaded] = useState(false);
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInputChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setSelectedFileName(selectedFile.name);
+      setFileUploaded(true);
+    } else {
+      setSelectedFileName("");
+      setFileUploaded(false);
+    }
+  };
+
+  const handleChooseAnotherClick = () => {
+    // Clear the selected file and open the file explorer again
+    setSelectedFileName("");
+    setFileUploaded(false);
+    fileInputRef.current.value = null; // Reset the file input
+    fileInputRef.current.click(); // Trigger a click event to open the file explorer
+  };
+
   return (
     <>
       <NavBar />
-      <main className=" m-10">
-        <Metric className="my-5">Edit Project</Metric>
+      <main className="m-10">
+        <Metric className="my-5 text-2xl">Edit Project</Metric>
 
-        <div className="border p-0 md:p-6 bg-slate-50">
-          <div>
-            <Title>
+        <div className="border p-4 md:p-6 bg-slate-100 rounded-lg shadow-md">
+          <div className="pb-4">
+            <Title className="text-xl font-semibold">
               <Bold>Drone : QI check 01201 BA</Bold>{" "}
             </Title>
-            <p>
-              Road quality inspection for NHAI project with the help of drones .
+            <p className="text-gray-600">
+              Road quality inspection for NHAI project with the help of drones.
             </p>
           </div>
 
-          <Card>
+          <Card className="p-4">
             <div>
-              <Title>
+              <Title className="text-lg font-semibold">
                 <Bold>Add more devices</Bold>{" "}
               </Title>
-              <p>
+              <p className="text-gray-600">
                 Add IoT devices as per the project needs. Happy customizing :)
               </p>
             </div>
 
-            <div className="relative h-[100%] my-2 border-1 border-dashed border-blue-400">
-              <Card className="flex justify-center items-center flex-col">
-                <span>
-                  <Button variant="light">Upload a file</Button> or drag and
-                  drop
-                </span>
-
-                <span>
-                  <Subtitle>.js/.ts up to 10MB</Subtitle>
-                </span>
-              </Card>
+            <div className="relative mt-4 h-32 border-2 border-dashed border-blue-400 rounded-lg p-4 flex flex-col justify-center items-center">
+              {fileUploaded ? (
+                <>
+                  <p className="mt-2 text-blue-500">
+                    Selected file: {selectedFileName}
+                  </p>
+                  <Button
+                    variant="light"
+                    onClick={handleChooseAnotherClick}
+                    className="mt-4"
+                  >
+                    Choose Another
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button variant="light" onClick={handleFileInputClick}>
+                    {selectedFileName ? "Choose Another" : "Upload a file"}
+                  </Button>
+                  <p className="mt-2 text-gray-600">or drag and drop</p>
+                  <Subtitle className="mt-2">.js/.ts</Subtitle>
+                  {/* Hidden file input */}
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                    onChange={handleFileInputChange}
+                  />
+                </>
+              )}
             </div>
           </Card>
 
-          <Card>
+          <Card className="mt-4 p-4">
             <Select>
               <SelectItem value="1">Up time components</SelectItem>
               <SelectItem value="2">Bar Chart Components</SelectItem>
--- frontend/src/components/IOTView/IOTinventory.jsx
+++ frontend/src/components/IOTView/IOTinventory.jsx
@@ -1,61 +1,78 @@
-import { RefreshIcon } from "@heroicons/react/outline";
+import { FiUploadCloud } from "react-icons/fi";
+import { IoDocumentAttachOutline } from "react-icons/io5";
+import { PiFiles } from "react-icons/pi";
+import { MdOutlineDelete } from "react-icons/md";
+import React from "react";
 import {
   Bold,
   Button,
   Card,
   Grid,
   Metric,
-  Select,
-  SelectItem,
-  Subtitle,
   Title,
 } from "@tremor/react";
-import React from "react";
 import NavBar from "../Navbar/Navbar";
 
 const IOTinventory = () => {
   return (
     <>
       <NavBar />
-      <div className=" m-10">
+      <div className="m-10">
         <Metric className="my-5">Inventory</Metric>
 
         <Grid
           numItems={6}
           numItemsMd={3}
-          className="gap-4 border   p-0 md:p-6 bg-slate-50"
+          className="gap-4 border p-0 md:p-6 bg-slate-50"
         >
           <Card>
-            <div>
-              <Title>
-                <Bold>Drone : QI check 01201 BA</Bold>{" "}
+            <div className="text-center">
+              <Title className="text-xl font-semibold">
+                Drone: QI check 01201 BA
               </Title>
-              <p>
-                Road quality inspection for NHAI project with the help of drones
-                .
+              <p className="text-gray-600 mt-2">
+                Road quality inspection for NHAI project using drones.
               </p>
             </div>
 
-            <div className="flex flex-col justify-evenly ">
-              <Button icon={RefreshIcon} className="my-2">
+            <div className="flex flex-col justify-evenly mt-4">
+              <Button
+                icon={FiUploadCloud}
+                className="my-2 transition-shadow hover:shadow-lg hover:bg-blue-100 hover:text-blue-500"
+              >
                 drone_script.js
               </Button>
-              <Button icon={RefreshIcon} className="my-2">
+              <Button
+                icon={IoDocumentAttachOutline}
+                className="my-2 transition-shadow hover:shadow-lg hover:bg-green-100 hover:text-green-500"
+              >
                 Attach Component
               </Button>
-              <Button icon={RefreshIcon} className="my-2">
+              <Button
+                icon={PiFiles}
+                className="my-2 transition-shadow hover:shadow-lg hover:bg-amber-100 hover:text-amber-500"
+                color="amber"
+              >
                 Log files
               </Button>
-              <Button icon={RefreshIcon} className="my-2">
+              <Button
+                icon={MdOutlineDelete}
+                className="my-2 transition-shadow hover:shadow-lg hover:bg-red-100 hover:text-red-500"
+                color="red"
+              >
                 Delete
               </Button>
             </div>
           </Card>
 
           <Card>
-            <div className="flex flex-col justify-center items-center">
-              <div>+</div>
-              <div>Add more devices to your inventory</div>
+            <div className="flex flex-col justify-center items-center h-full">
+              <div className="text-4xl text-blue-500 cursor-pointer transform hover:scale-105 transition-transform">
+                +
+              </div>
+              <div className="text-center font-semibold text-gray-700">
+                Add Device
+              </div>
             </div>
           </Card>
         </Grid>
