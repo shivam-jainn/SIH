efa36ec49e4b6d29151319b29ca4fa0169666e3d
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-15 11:35:18 +0530

    Add Backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Socket setup - require socketHandlers after defining handleSocketConnection
+const { handleSocketConnection } = require("./socketHandlers");
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+    handleSocketConnection(socket);
+});
