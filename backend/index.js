86887ce960cd29ca2fff141d88ebbd4107fc0aab
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-15 06:15:17 +0530

    Add postman test

--- backend/index.js
+++ backend/index.js
@@ -15,28 +15,53 @@ const io = new Server(server, {
   },
 });
 
+// Simulate machine data
+let machineData = {
+  temperature: 0,
+  vibration: 0,
+  soundWaves: 0,
+  humidity: 0,
+  surroundingTemp: 0,
+};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  // Simulate machine data and send it to the client every 2 seconds
-  setInterval(() => {
-    const machineData = {
+  // Send machine data to the client every 2 seconds
+  const machineDataInterval = setInterval(() => {
+    // Update machine data
+    machineData = {
       temperature: Math.random() * 100,
       vibration: Math.random() * 10,
       soundWaves: Math.random() * 1000,
       humidity: Math.random() * 100,
       surroundingTemp: Math.random() * 100,
     };
-    socket.emit("machine_data", machineData);
+    // Emit the updated machine data to all clients
+    io.emit("machine_data", machineData);
   }, 2000); // Update data every 2 seconds
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+  });
 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
+  socket.on("send_message", (data) => {
+    // Broadcast the message to everyone in the same room
+    io.to(data.room).emit("receive_message", {
+      user: socket.id, // You can use socket.id as the user for demonstration purposes
+      message: data.message,
     });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected: ${socket.id}`);
+    clearInterval(machineDataInterval); // Clear the machine data interval on disconnect
+  });
+});
+
+// Define an HTTP endpoint to get machine data
+app.get("/api/machine", (req, res) => {
+  res.json(machineData);
 });
 
 server.listen(3001, () => {
