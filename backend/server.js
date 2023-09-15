7723250849bab29f6c7cdce8a2fe89d24c8eeb92
shivam-jainn <shivam.clgstash@gmail.com>
2023-09-15 08:55:22 +0530

    socket handler and server . refactored

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,30 @@
+// server.js
 const express = require("express");
-const socket = require("socket.io");
-
-
-const PORT = 3000;
+const http = require("http");
+const socketIo = require("socket.io");
 const app = express();
-const server = app.listen(PORT, function () {
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = 3000;
+
+const { handleSocketConnection } = require("./socketHandlers");
+
+
+server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
 
-
 app.use(express.static("public"));
 
-// Socket setup
-const io = socket(server);
-const activeUsers = new Set();
-io.on("connection", function (socket) {
-    console.log("Made socket connection");
-
-
-    socket.on("iot", function (data) {
-        // socket.to("commonroom").emit("iot",data);
-        socket.broadcast.emit("iot",data);
-        console.log(data);
-    });
-
-    socket.on("admin",(data)=>{
-        socket.join("commonroom");
-    })
-
-
-
-    socket.on("message", function (data) {
-        console.log(data);
-    });
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 
-io.on("addExtension",(socket,deviceid)=>{
-    console.log("joined dashboard");
-    console.log(deviceid);
-    io.join("dashboard");
-})
+// Socket setup
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+    handleSocketConnection(socket);
+});
---
+++ backend/socketHandlers.js
@@ -0,0 +1,54 @@
+// socketHandlers.js
+const ADMIN_ROOM = "admin_room";
+const MANAGER_ROOM = "manager_room";
+const COMMON_ROOM = "common_room";
+
+const deviceSockets = {};
+
+const device_meta = {};
+
+function removeDeviceFromRoom(socket, roomName, deviceId) {
+  if (device_meta[deviceId] && device_meta[deviceId].rooms.includes(roomName)) {
+    socket.leave(roomName);
+    console.log(`Device ${deviceId} left ${roomName} room`);
+    device_meta[deviceId].rooms = device_meta[deviceId].rooms.filter(
+      (room) => room !== roomName
+    );
+  }
+
+  function handleSocketConnection(socket) {
+    if (socket.handshake.headers.isdevice == 1) {
+      socket.join("dashboard");
+      console.log("joined dashboard");
+    }
+
+    socket.on("device_register", (deviceId, ...rooms) => {
+      // Store the device socket using its ID
+
+      deviceSockets[deviceId] = socket;
+
+      rooms.forEach((room) => {
+        socket.join(room);
+        console.log(`Device ${deviceId} joined room ${room}`);
+      });
+    });
+
+    socket.on("iot", (data) => {
+      // socket.to("commonroom").emit("iot", data);
+      socket.broadcast.emit("iot", data);
+      console.log(data);
+    });
+
+   
+    socket.on("remove_device", (data) => {
+      const { roomName, deviceId } = data;
+      removeDeviceFromRoom(socket, roomName, deviceId);
+    });
+
+    socket.on("message", (data) => {
+      console.log(data);
+    });
+  }
+}
+
+module.exports = { handleSocketConnection };
