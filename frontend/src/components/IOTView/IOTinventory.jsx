c3e050d69156c813d3aa16fadc9ed6e4598a0599
shivam-jainn <shivam.clgstash@gmail.com>
2023-09-15 11:06:02 +0530

    IOT inventory updated

--- frontend/src/components/IOTView/IOTinventory.jsx
+++ frontend/src/components/IOTView/IOTinventory.jsx
@@ -9,22 +9,7 @@ const IOTinventory = () => {
       <main>
         <Title>Inventory</Title>
 
-        <Grid numItems={6} className="gap-4">
-
-        <Card>
-                  <div>
-                  <Title><Bold>Drone : QI check 01201 BA</Bold> </Title>
-                  <p>Road quality inspection for NHAI project with the help of drones .</p>
-                  </div>
-
-
-    <div className="flex flex-col justify-evenly ">
-        <Button icon={RefreshIcon} className="my-2" >Deploy Data Scripts</Button>
-        <Button icon={RefreshIcon} className="my-2">Manage Component</Button>
-        <Button icon={RefreshIcon} className="my-2">Place issue</Button>
-    </div>
-
-                </Card>
+        <Grid numItems={6} numItemsMd={3} className="gap-4">
 
                   <Card>
                   <div>
@@ -34,12 +19,29 @@ const IOTinventory = () => {
 
 
     <div className="flex flex-col justify-evenly ">
-        <Button icon={RefreshIcon} className="my-2" >Deploy Data Scripts</Button>
-        <Button icon={RefreshIcon} className="my-2">Manage Component</Button>
-        <Button icon={RefreshIcon} className="my-2">Place issue</Button>
+        <Button icon={RefreshIcon} className="my-2" >drone_script.js</Button>
+        <Button icon={RefreshIcon} className="my-2">Attach Component</Button>
+        <Button icon={RefreshIcon} className="my-2">Log files</Button>
+        <Button icon={RefreshIcon} className="my-2">Delete</Button>
+
     </div>
 
                 </Card>
+
+
+
+
+        <Card>
+                <div className="flex flex-col justify-center items-center">
+                    <div>+</div>
+                    <div>
+                Add more devices to your inventory
+
+                    </div>
+                </div>
+
+                </Card>
+
         </Grid>
       </main>
     </>
