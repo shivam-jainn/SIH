0f4c91c3262c7ce8fb3922b70305cb25beb7fef8
shivam-jainn <shivam.clgstash@gmail.com>
2023-09-15 15:17:34 +0530

    key features

--- frontend/src/components/LandingView/LandingPage.jsx
+++ frontend/src/components/LandingView/LandingPage.jsx
@@ -29,7 +29,51 @@ const LandingPage = (props) => {
           <u>streamline construction</u>
         </span>
       </section>
+
+    
       <CopyPaste1 />
+
+
+        {/* Feature Section */}
+        <section className="bg-white py-12">
+        <div className="max-w-6xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-gray-800 mb-6">
+            Key Features
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {/* Feature 1 */}
+            <div className="p-6 border rounded-lg shadow-lg bg-white">
+              <h3 className="text-xl font-semibold text-gray-700 mb-2">
+                Plug-and-Play Dashboard
+              </h3>
+              <p className="text-gray-600">
+                Our platform offers a plug-and-play dashboard solution, making it easy to set up and customize according to your needs.
+              </p>
+            </div>
+
+            {/* Feature 2 */}
+            <div className="p-6 border rounded-lg shadow-lg bg-white">
+              <h3 className="text-xl font-semibold text-gray-700 mb-2">
+                Highly Scalable
+              </h3>
+              <p className="text-gray-600">
+                BricBoard is highly scalable, allowing you to adapt and grow your construction projects without limitations.
+              </p>
+            </div>
+
+            {/* Feature 3 */}
+            <div className="p-6 border rounded-lg shadow-lg bg-white">
+              <h3 className="text-xl font-semibold text-gray-700 mb-2">
+                Editable & Customizable
+              </h3>
+              <p className="text-gray-600">
+                You can easily edit and customize our platform to match your construction project's unique requirements.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 };
